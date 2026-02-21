68de09b2db365815e7e64e789bf9dd188113d001
Mark Mikkelsen <mark.mikkelsen@gmail.com>
2026-02-21 11:07:06 -0500

    Style cleanup: run_mysLASERShaped_fast & generate
    Miscellaneous formatting and minor cleanups:
    - Normalized whitespace and comment formatting in dependencies/run_mysLASERShaped_fast.m
    - Simplified function signature to 'function out = run_mysLASERShaped_fast(...)' and removed extra brackets/spacing
    - Standardized progress-bar printing and loop indentation/spacing in parfor/for loops
    - Tidied up loop bodies, alignment, and removed extraneous blank lines/trailing spaces
    - Replaced 'clear all;' with 'clear;' in run_generate_my_mets.m
    No intended functional logic changes; these are cosmetic and readability improvements.

--- dependencies/run_mysLASERShaped_fast.m
+++ dependencies/run_mysLASERShaped_fast.m
@@ -5,35 +5,35 @@
 
 % USAGE:
 % out=run_simPressShaped_fast(spinSys);
-% 
+%
 % DESCRIPTION:
-% This script simulates a PRESS experiment with fully shaped refocusing 
+% This script simulates a PRESS experiment with fully shaped refocusing
 % pulses. Coherence order filtering is employed to only simulate desired signals
 % This results in a 4x speed up compared to phase cycling (see deprecated run_simSemiLASERShaped_fast_phCyc.m)
-% Furthermore, simulations are run at various locations in space to account for the 
-% within-voxel spatial variation of the metabolite signal.  Summation 
-% across spatial positions is performed. The MATLAB parallel computing toolbox 
-% (parfor loop) was used to accelerate the simulations.  Acceleration 
+% Furthermore, simulations are run at various locations in space to account for the
+% within-voxel spatial variation of the metabolite signal.  Summation
+% across spatial positions is performed. The MATLAB parallel computing toolbox
+% (parfor loop) was used to accelerate the simulations.  Acceleration
 % is currently performed in the direction of the slice selective pulse along
 % the x-direction, but this can be changed.  Up to a factor of 12 acceleration
-% can be achieved using this approach. To achieve 
+% can be achieved using this approach. To achieve
 % faster perfomance compared to the original 'run_simSemiLASER_shaped.m' function,
-% this code uses the method described by Yan Zhang et al. Med Phys 2017;44(8): 
-% 4169-78.  Some additional acceleration is currently performed using parfor 
+% this code uses the method described by Yan Zhang et al. Med Phys 2017;44(8):
+% 4169-78.  Some additional acceleration is currently performed using parfor
 % loops in both x and y directions.  To enable the use of the MATLAB
 % parallel computing toolbox, initialize the multiple worked nodes using
 % "matlabpool size X" where "X" is the number of available processing
 % nodes.  If the parallel processing toolbox is not available, then replace
 % the "parfor" loop with a "for" loop.
-% 
-function [ out] = run_mysLASERShaped_fast(rfPulse,refTp,Npts,sw,lw,Bfield,thkX,thkY,x,y,te,sys,flipAngle)
+%
+function out = run_mysLASERShaped_fast(rfPulse,refTp,Npts,sw,lw,Bfield,thkX,thkY,x,y,te,sys,flipAngle)
 %function out=run_simPressShaped_fast(spinSys)
 
 % INPUTS:
 % To run this script, there is technically only one input argument:
-% spinSys           = spin system to simulate 
+% spinSys           = spin system to simulate
 %
-% However, the user should also edit the following parameters as 
+% However, the user should also edit the following parameters as
 % desired before running the function:
 % refocWaveform     = name of refocusing pulse waveform.
 % refTp             = duration of refocusing pulses[ms]
@@ -72,7 +72,7 @@ centreFreq=sys(1).centreFreq;
 rfPulse=rf_resample(rfPulse,100);
 %
 if ~rfPulse.isGM
-    %Non-gradient modulated pulse - Calculating the x and y gradient 
+    %Non-gradient modulated pulse - Calculating the x and y gradient
     %strengths for the desired slice thickness
     Gx=(rfPulse.tbw/(refTp/1000))/(gamma*thkX/10000); %[G/cm]
     Gy=(rfPulse.tbw/(refTp/1000))/(gamma*thkY/10000); %[G/cm]
@@ -99,7 +99,7 @@ d=cell(1,1);
 
 % parfor X=1:length(x)
 % %  for X=1:length(x)
-% 
+%
 %        % disp(['Executing X-position ' num2str(X) '!!' ]);
 %         out_posx_rpc{X}=sim_sLASER_shaped_Ref1(Bfield,sys,te,rfPulse,refTp,x(X),Gx,flipAngle,centreFreq);
 % %                            sim_sLASER_shaped_Ref1(Bfield,sys,te,RF,       tp,  dx, Gx,ph1,   ph2,  centreFreq)
@@ -112,10 +112,10 @@ numIterations = length(x);
 parfor X = 1:numIterations
     % Calculate the progress percentage
     progressPercentage = X / numIterations * 100;
-    
+
     % Create a progress bar string
     progressBar = ['[', repmat('=', 1, round(progressPercentage)), repmat(' ', 1, 100 - round(progressPercentage)), ']'];
-    
+
     % Display the progress bar in the command window
     fprintf('Progress X-position: %s %.1f%%\r', progressBar, progressPercentage);
 
@@ -130,7 +130,7 @@ fprintf('\n');
 %calculate the average density matrix (Doing this inside a separate for
 %loop because I couldn't figure out how to do this inside the parfor loop):
 for X=1:length(x)
-        d{1}=sim_dAdd(d{1},out_posx_rpc{X});
+    d{1}=sim_dAdd(d{1},out_posx_rpc{X});
 end
 
 % %Initialize structures:
@@ -139,18 +139,18 @@ out=struct([]);
 
 %Now loop through y direction (second refoc pulse only);
 parfor Y=1:length(y) %Use this if you do have the MATLAB parallel processing toolbox
-%for Y=1:length(y) %Use this if you don't have the MATLAB parallel processing toolbox
- % Calculate the progress percentage
+    %for Y=1:length(y) %Use this if you don't have the MATLAB parallel processing toolbox
+    % Calculate the progress percentage
     progressPercentage = Y / numIterations * 100;
-  % Create a progress bar string
+    % Create a progress bar string
     progressBar = ['[', repmat('=', 1, round(progressPercentage)), repmat(' ', 1, 100 - round(progressPercentage)), ']'];
-    
+
     % Display the progress bar in the command window
     fprintf('Progress Y-position: %s %.1f%%\r', progressBar, progressPercentage);
-     %   disp(['Executing Y-position ' num2str(Y) '!!' ]);
-     % Perform your computation here
-        out_posy_rpc{Y}=sim_sLASER_shaped_Ref2(d{1},Npts,sw,Bfield,lw,sys,te,rfPulse,refTp,y(Y),Gy,flipAngle,centreFreq);
-%                            sim_sLASER_shaped_Ref2(d,   n,sw,Bfield,linewidth,sys,te,RF,       tp, dy,  Gy,ph3,    ph4,  centreFreq)
+    %   disp(['Executing Y-position ' num2str(Y) '!!' ]);
+    % Perform your computation here
+    out_posy_rpc{Y}=sim_sLASER_shaped_Ref2(d{1},Npts,sw,Bfield,lw,sys,te,rfPulse,refTp,y(Y),Gy,flipAngle,centreFreq);
+    %                            sim_sLASER_shaped_Ref2(d,   n,sw,Bfield,linewidth,sys,te,RF,       tp, dy,  Gy,ph3,    ph4,  centreFreq)
 end
 
 % Print a new line to clear the progress bar
@@ -160,7 +160,7 @@ fprintf('\n');
 %Now combine the outputs;  Again, doing this inside a separate for loop
 %becuase I can't figure out how to do this inside the parfor loop:
 for Y=1:length(y)
-        out=op_addScans(out,out_posy_rpc{Y});
+    out=op_addScans(out,out_posy_rpc{Y});
 end
 
 
@@ -177,19 +177,3 @@ out=op_ampScale(out,1/voxRatio);
 
 
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-       
